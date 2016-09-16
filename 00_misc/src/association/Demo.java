528daeff2c5b809ddb4e6e6bb4c882850b19387a
Mike Burton <mikeb@mycosystems.co.uk>
2016-09-16 12:14:55 +0100

    Misc

--- 00_misc/src/association/Demo.java
+++ 00_misc/src/association/Demo.java
@@ -5,16 +5,16 @@ public class Demo {
 	public static void main(String[] args) {
 
 		// Step 1 (Add ctor)
-		House h= new House("Burton Towers", 3);	
+		House h= new House( "Burton Towers", 3 );	
 		System.out.println( h );
 
 		// Step 2
-		Door front= new Door( "Front", 82, true);
+		Door front= new Door( "Front", 82, true );
 		System.out.println( front );
 
 		// Step 3
 		h.addDoor(front);
-		h.addDoor( new Door("Back", 72, false));
+		h.addDoor( new Door("Back", 72, false) );
 		System.out.println( h );
 
 	}
